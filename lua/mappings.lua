01f699702d662375f134eb056b97bf89153b8370
Hans Hoffman <hoffman.hans.j@gmail.com>
2022-01-08 12:08:54 -0500

    transfer over old settings

---
+++ lua/mappings.lua
@@ -0,0 +1,24 @@
+local function map(mode, lhs, rhs, opts)
+  local options = { noremap = true, silent = true }
+
+  if opts then
+    options = vim.tbl_extend("force", options, opts)
+  end
+
+  vim.api.nvim_set_keymap(mode, lhs, rhs, options)
+end
+
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+-- Better window navigation
+map("n", "<C-h>", "<C-w>h")
+map("n", "<C-j>", "<C-w>j")
+map("n", "<C-k>", "<C-w>k")
+map("n", "<C-l>", "<C-w>l")
+
+-- Resize windows with arrow keys
+map("n", "<C-Up>", ":resize +2<CR>")
+map("n", "<C-Down>", ":resize -2<CR>")
+map("n", "<C-Left>", ":vertical resize -2<CR>")
+map("n", "<C-Right>", ":vertical resize +2<CR>")
---
+++ lua/options.lua
@@ -0,0 +1,29 @@
+local temp_dir = "~/.tmp,/private/tmp//"
+local options = {
+  backupdir      = temp_dir,       -- store backup files here instead of next to file
+  clipboard      = "unnamedplus",  -- copy paste between vim and everything else
+  cmdheight      = 2,              -- increase number of lines to help with displaying messages
+  columns        = 100,            -- number of chars at which to show the wrap guide (max line length)
+  directory      = temp_dir,       -- store swap files here instead of next to file
+  expandtab      = true,           -- convert tabs to spaces
+  mouse          = "a",            -- enable mouse in all modes
+  number         = true,           -- show line numbers
+  relativenumber = true,           -- show relative line number
+  ruler          = false,          -- no need to show line, col numbers of cursor position
+  shiftwidth     = 2,              -- number of spaces inserted for each indentation
+  signcolumn     = "yes",          -- always display "gutter" to prevent shifting
+  smartcase      = true,           -- be case sensitive during searches
+  smartindent    = true,           --
+  splitbelow     = true,           -- horizontal splits will go below the current window
+  splitright     = true,           -- vertical splits will go right of the current window
+  swapfile       = false,          --
+  tabstop        = 2,              -- number of spaces inserted for a tab
+  undodir        = temp_dir,       -- store undo files here instead of next to file
+  wrap           = false,          -- display lines as one long line
+}
+
+for k, v in pairs(options) do
+  vim.opt[k] = v
+end
+
+vim.opt.iskeyword:append({"-"})   -- treat dash separated words as a word
