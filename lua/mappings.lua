f14e3490bc79a9733f16fe87ea0ee91724569a75
Hans Hoffman <hoffman.hans.j@gmail.com>
2022-01-08 12:27:21 -0500

    add visual indentation

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -1,11 +1,11 @@
-local function map(mode, lhs, rhs, opts)
+local function map(mode, key, action, extra)
   local options = { noremap = true, silent = true }
 
-  if opts then
-    options = vim.tbl_extend("force", options, opts)
+  if extra then
+    options = vim.tbl_extend("force", options, extra)
   end
 
-  vim.api.nvim_set_keymap(mode, lhs, rhs, options)
+  vim.api.nvim_set_keymap(mode, key, action, options)
 end
 
 vim.g.mapleader = " "
@@ -22,3 +22,7 @@ map("n", "<C-Up>", ":resize +2<CR>")
 map("n", "<C-Down>", ":resize -2<CR>")
 map("n", "<C-Left>", ":vertical resize -2<CR>")
 map("n", "<C-Right>", ":vertical resize +2<CR>")
+
+-- Reselect visual selection after indenting
+map("v", "<", "<gv")
+map("v", ">", ">gv")
