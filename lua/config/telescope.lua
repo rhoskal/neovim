cb78cceb7329007a1fed1623778e8a5e82ebf85b
Hans Hoffman <hoffman.hans.j@gmail.com>
2022-01-08 19:06:49 -0500

    make keymap function global

---
+++ lua/config/telescope.lua
@@ -0,0 +1,2 @@
+keymap("n", "<leader>ff", ":Telescope find_files<CR>")
+keymap("n", "<leader>fg", ":Telescope live_grep<CR>")
--- lua/mappings.lua
+++ lua/mappings.lua
@@ -1,4 +1,4 @@
-local function map(mode, key, action, extra)
+function keymap(mode, key, action, extra)
   local options = { noremap = true, silent = true }
 
   if extra then
@@ -12,32 +12,28 @@ vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
 -- Better window navigation
-map("n", "<C-h>", "<C-w>h")
-map("n", "<C-j>", "<C-w>j")
-map("n", "<C-k>", "<C-w>k")
-map("n", "<C-l>", "<C-w>l")
+keymap("n", "<C-h>", "<C-w>h")
+keymap("n", "<C-j>", "<C-w>j")
+keymap("n", "<C-k>", "<C-w>k")
+keymap("n", "<C-l>", "<C-w>l")
 
 -- Resize windows with arrow keys
-map("n", "<C-Up>", ":resize +2<CR>")
-map("n", "<C-Down>", ":resize -2<CR>")
-map("n", "<C-Left>", ":vertical resize -2<CR>")
-map("n", "<C-Right>", ":vertical resize +2<CR>")
+keymap("n", "<C-Up>", ":resize +2<CR>")
+keymap("n", "<C-Down>", ":resize -2<CR>")
+keymap("n", "<C-Left>", ":vertical resize -2<CR>")
+keymap("n", "<C-Right>", ":vertical resize +2<CR>")
 
 -- Reselect visual selection after indenting
-map("v", "<", "<gv")
-map("v", ">", ">gv")
+keymap("v", "<", "<gv")
+keymap("v", ">", ">gv")
 
 -- Allow "go-to-file" to open non-existent files
-map("", "gf", ":edit <cfile><CR>")
+keymap("", "gf", ":edit <cfile><CR>")
 
 -- Easy buffer switching
-map("v", "<TAB>", ":bnext<CR>")
-map("v", "<S-TAB>", ":bprevious<CR>")
+keymap("v", "<TAB>", ":bnext<CR>")
+keymap("v", "<S-TAB>", ":bprevious<CR>")
 
 -- Move selected line / block of text in visual mode
-map("x", "K", ":move '<-2<CR>gv-gv")
-map("x", "J", ":move '>+1<CR>gv-gv")
-
--- Telescope
-map("n", "<leader>ff", ":Telescope find_files<CR>")
-map("n", "<leader>fg", ":Telescope live_grep<CR>")
+keymap("x", "K", ":move '<-2<CR>gv-gv")
+keymap("x", "J", ":move '>+1<CR>gv-gv")
