a0cc9b612957f599caef40b1b785963a1717c322
Hans Hoffman <hoffman.hans.j@gmail.com>
2022-01-08 21:00:00 -0500

    bubble lualine

--- lua/config/lualine.lua
+++ lua/config/lualine.lua
@@ -8,6 +8,58 @@ end
 
 lualine.setup {
   options = {
-    theme = "tokyonight"
-  }
+    theme = "tokyonight",
+    component_separators = "|",
+    section_separators = {
+      left = "",
+      right = "",
+    },
+  },
+  sections = {
+    lualine_a = {
+      {
+        "mode",
+        separator = {
+          left = ""
+        },
+        right_padding = 2
+      },
+    },
+    lualine_b = { "filename", "branch" },
+    lualine_c = {},
+    lualine_x = {},
+    lualine_y = {
+      {
+        "diagnostics",
+        sources = { "nvim_diagnostic" },
+        sections = {
+          "error",
+          "warn",
+          "info",
+          "hint",
+        },
+        update_in_insert = false,
+        always_visible = true,
+      },
+    },
+    lualine_z = {
+      {
+        "location",
+        separator = {
+          right = "",
+        },
+        left_padding = 2,
+      },
+    },
+  },
+  inactive_sections = {
+    lualine_a = { "filename" },
+    lualine_b = {},
+    lualine_c = {},
+    lualine_x = {},
+    lualine_y = {},
+    lualine_z = { "location" },
+  },
+  tabline = {},
+  extensions = {},
 }
