d5a3b6841155b476a28a7a47aac17d646651324e
Hans Hoffman <hoffman.hans.j@gmail.com>
2022-01-09 19:00:08 -0500

    nvim-tree respect git ignore

--- lua/config/explorer.lua
+++ lua/config/explorer.lua
@@ -8,6 +8,10 @@ end
 
 vim.g.nvim_tree_indent_markers = 1
 vim.g.nvim_tree_highlight_opened_files = 1
+vim.g.vim_tree_special_files = {
+  "README.md",
+  "Makefile",
+}
 
 explorer.setup {
   auto_close = true,
@@ -15,8 +19,13 @@ explorer.setup {
     dotfiles = true,
   },
   git = {
-    enable = false,
+    enable = true,
+    ignore = true,
   },
+  trash = {
+    cmd = "trash",
+    require_confirm = true,
+  }
 }
 
 keymap("n", "<leader>fd", ":NvimTreeToggle<CR>")
