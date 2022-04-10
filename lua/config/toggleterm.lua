906f26c29be31bddad115c3c9a273dd885a8d623
Hans Hoffman <hoffman.hans.j@gmail.com>
2022-04-10 14:10:32 -0600

    toggleterm keymappings

--- lua/config/toggleterm.lua
+++ lua/config/toggleterm.lua
@@ -14,7 +14,7 @@ terminal.setup {
       return vim.o.columns * 0.4
     end
   end,
-  open_mapping = [[;;]],
+  open_mapping = [[<C-\>]],
   hide_numbers = true,
   shade_filetypes = {},
   shade_terminals = true,
@@ -50,3 +50,14 @@ local lazygit = Terminal:new({
 function _lazygit_toggle()
   lazygit:toggle()
 end
+
+function _set_terminal_keymaps()
+  local opts = { noremap = true }
+  vim.api.nvim_buf_set_keymap(0, "t", "<esc>", [[<C-\><C-n>]], opts)
+  vim.api.nvim_buf_set_keymap(0, "t", "<C-h>", [[<C-\><C-n><C-W>h]], opts)
+  vim.api.nvim_buf_set_keymap(0, "t", "<C-j>", [[<C-\><C-n><C-W>j]], opts)
+  vim.api.nvim_buf_set_keymap(0, "t", "<C-k>", [[<C-\><C-n><C-W>k]], opts)
+  vim.api.nvim_buf_set_keymap(0, "t", "<C-l>", [[<C-\><C-n><C-W>l]], opts)
+end
+
+vim.cmd("autocmd! TermOpen term://* lua _set_terminal_keymaps()")
